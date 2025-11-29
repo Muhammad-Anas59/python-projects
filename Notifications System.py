72b01a983da77201dffe0ece3254b48c834d865c
Muhammad Anas <muhammadanasehsan@gmail.com>
2025-11-30 03:18:32 +0500

    Update Notifications System.py

--- Notifications System.py
+++ Notifications System.py
@@ -1,13 +1,11 @@
 from win10toast import ToastNotifier
 import pyttsx3
 
-# --- Notification ---
 toast = ToastNotifier()
 toast.show_toast("Reminder",
-                 "Hey Anas, drink water 💧",
+                 "Hey Anas, drink water ",
                  duration=5)
 
-# --- Voice Speech ---
 engine = pyttsx3.init()
 engine.say("Hey Anas, drink water")
 engine.runAndWait()
